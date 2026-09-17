7b347ff431198bf437746941d454788639ec3f6d
caroleena33glitch <carolina.essa33@gmail.com>
2026-09-17 13:18:08 -0600

    Update streamlit_app.py
    code correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,13 @@ st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_contaniner_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
+
+
+
+
+
+
